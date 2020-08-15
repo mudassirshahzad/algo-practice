6a0b39bd7ac21a1cceabccbe0c7b9e0420dae56a
Mudassir Shahzad <mudassir.shahzad@dnata.com>
2020-08-15 04:31:45 +0400

    Changed visibility of data field of Node class

--- Algos/src/main/java/com/mudassirshahzad/algos/utils/Node.java
+++ Algos/src/main/java/com/mudassirshahzad/algos/utils/Node.java
@@ -3,7 +3,7 @@ package com.mudassirshahzad.algos.utils;
 public class Node<T extends Comparable<?>> {
     public Node<T> left;
     public Node<T> right;
-    T data;
+    public T data;
 
     public Node(T data) {
         this.data = data;
