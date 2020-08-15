099a8a681914c0af68d142fced7acaffd5ccd44b
Mudassir Shahzad <mudassir.shahzad@dnata.com>
2020-08-15 04:32:17 +0400

    Added algo for longest palindromic substring

---
+++ Algos/src/main/java/com/mudassirshahzad/algos/LongestPalindromicSubstring.java
@@ -0,0 +1,52 @@
+package com.mudassirshahzad.algos;
+
+class LongestPalindromicSubstring {
+
+    public static void main(String... strings) {
+
+        String inputString = "aaaaaabsskkddfjffjasflkjsflkjasfljsdflkjasljsflskjfracecarslkjsdflkjjsdfkhsdflkhsdflkhasflkhasdflkjbbabbabb";
+        // String inputString = "racecar";
+
+        String subString = new LongestPalindromicSubstring().longestPalindrome(inputString);
+        System.out.println("Longest palindromic substring = " + subString);
+
+    }
+
+    int resultStart;
+    int resultLength;
+
+    public String longestPalindrome(String s) {
+        int strLength = s.length();
+        if (strLength < 2) {
+            return s;
+        }
+
+        // Iterate through the string and check whether each character is a possible center of a possible palindrome
+        for (int start = 0; start < strLength; start++) {
+            expandRange(s, start, start);
+            expandRange(s, start, start + 1);
+        }
+
+        return s.substring(resultStart, resultStart + resultLength);
+    }
+
+    int count = 0;
+
+    private void expandRange(String str, int begin, int end) {
+
+        count++;
+
+        while (begin >= 0 && end < str.length() &&
+            str.charAt(begin) == str.charAt(end)) {
+
+            begin--;
+            end++;
+        }
+
+        if (resultLength < end - begin - 1) {
+            resultStart = begin + 1;
+            resultLength = end - begin - 1;
+        }
+
+    }
+}
---
+++ Algos/src/main/resources/LongestPalindromicSubstring.md
@@ -0,0 +1,11 @@
+### Problem Statement - Treasure Island
+
+https://leetcode.com/problems/longest-palindromic-substring/solution
+
+## Summary
+
+
+This article is for intermediate readers. It introduces the following ideas: 
+Palindrome, Dynamic Programming and String Manipulation. Make sure you understand 
+what a palindrome means. A palindrome is a string which reads the same in both directions. 
+For example, SS = "aba" is a palindrome, SS = "abc" is not
